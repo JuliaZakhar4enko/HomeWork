973d259525caeb49959da7a6c7150c231159ecb0
Yulia <yulia.zakhar4enko@gmail.com>
2023-06-01 12:06:30 +0300

    Добавила папку HomeWork_007

---
+++ HomeWork_007/Program.cs
@@ -0,0 +1,207 @@
+﻿//Урок 7. Двумерные массивы
+
+/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+/*
+double[,] GetArray(int m, int n)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = Math.Round(new Random().NextDouble(), 1) + new Random().Next(-9, 10);
+        }
+    }
+    return result;
+}
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+double[,] array = GetArray(rows, columns);
+PrintArray(array);
+*/
+
+
+/*
+Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве, 
+и возвращает позицию этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет
+*/
+
+/*
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FindElement(int[,] inArray)
+{
+    Console.Write("Введите индекс строки массива: ");
+    int x = int.Parse(Console.ReadLine()!);
+    Console.Write("Введите индекс столбца массива: ");
+    int y = int.Parse(Console.ReadLine()!);
+    int count = 0;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (x <= i & y <= j)
+            {
+                count++;
+            }
+        }
+    }
+    if (count > 0)
+    {
+        Console.WriteLine($"Число с индексами [{x};{y}] существует и равно {inArray[x, y]}");
+    }
+    else { Console.WriteLine($"Число с индексами [{x};{y}] не существует"); }
+}
+
+int[,] array = GetArray(3, 4, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+FindElement(array);
+*/
+
+
+/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write(inArray[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SumAvg(int[,] inArray)
+{
+    double sumAverage = 1;
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < inArray.GetLength(0); i++)
+        {
+            sum = sum + inArray[i, j];
+            sumAverage = sum / inArray.GetLength(0);
+        }
+        Console.Write(Math.Round(sumAverage, 1) + "; ");
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов массива: ");
+int colomns = int.Parse(Console.ReadLine()!);
+int[,] array = GetArray(rows, colomns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+Console.Write("Среднее арифметическое каждого столбца: ");
+SumAvg(array);
+
+
+
+
+/*
+Задача HARD SORT необязательная. Считается за три обязательных
+Задайте двумерный массив из целых чисел. Количество строк и столбцов задается с клавиатуры. Отсортировать элементы по возрастанию слева направо и сверху вниз.
+
+Например, задан массив:
+1 4 7 2
+5 9 10 3
+
+После сортировки
+1 2 3 4
+5 7 9 10
+*/
+
+/*
+задача 2 HARD необязательная. Считается за четыре обязательных ) то есть три этих и одна с будущего семинара
+Сгенерировать массив случайных целых чисел размерностью m*n (размерность вводим с клавиатуры) , причем чтоб количество элементов было четное. Вывести на экран красивенько таблицей. Перемешать случайным образом элементы массива, причем чтобы каждый элемент гарантированно и только один раз переместился на другое место и выполнить перемешивание за m*n / 2 итераций. То есть если массив три на четыре, то надо выполнить за 6 итераций. И далее в конце опять вывести на экран как таблицу.
+например, на входе массив 2* 3
+2 3 5
+1 9 4
+первая итерация перемешивания - выбрали индексы 0,0 и 1,2 случайный образом, поменяли ячейки
+4 3 5
+1 9 2
+вторая итерация - индексы 0, 0 и 1,2 не трогаем, так как это уже перемещенные ячейки, выбираем случайным образом из оставшихся ячеек, выбрали ячейки 0, 1 и 0,2, поменяли значения
+4 5 3
+1 9 2
+третья итерация - теперь остались только ячейки 1,0 и 1,1, меняем их местами
+4 5 3
+9 1 2
+*/
+
+
+
+
